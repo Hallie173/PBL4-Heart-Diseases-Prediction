f3a26c97d6a9a3f54621c99191ef5462cde0bb20
g15 5511 <phuonghaleduy@gmail.com>
2024-10-09 21:21:50 +0700

    daily commit

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,31 +1,25 @@
 import React from "react";
-import "./LogIn.css";
+import styles from "./LogIn.module.css";
+import { Link } from 'react-router-dom';
 
 function LogIn() {
     return (
-        <div className="login">
-        <form>
-          <h1>Log In</h1>
-          <div className="form-control">
-              <input id="first-name" type="text" placeholder="First Name" />
-              <small></small>
-              <span></span>
-          </div>
-          <div className="form-control">
-              <input id="Last Name" type="text" placeholder="Last Name" />
-              <small></small>
-              <span></span>
-          </div>
-          <div className="form-control">
+        <div className={styles.login}>
+        <form className={styles.loginForm}>
+          <div className={styles.title}><h1>Log In</h1></div>
+          <div className={styles.formControl}>
               <input id="email" type="email" placeholder="Email" />
               <small></small>
               <span></span>
           </div>
-          <div className="form-control">
+          <div className={styles.formControl}>
               <input id="password" type="password" placeholder="Password" />
               <small></small>
               <span></span>
           </div>
+          <button type="submit" className={styles.submitButton}>Log In</button>
+          <div className={styles.forgetPassword}><a href="#">Forget password?</a></div>
+          <div className={styles.signupNow}><Link to="/signup" className={styles.signupLink}>Sign Up now!</Link></div>
         </form>
       </div>
     );
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,59 +1,59 @@
 import React from "react";
-import "./SignUp.css";
+import styles from "./SignUp.module.css";
+import { Link } from 'react-router-dom';
 
 function SignUp() {
     return (
-        <div className="signup">
-        <form>
-            <h1>Sign Up</h1>
-            <div className="form-control">
+        <div className={styles.signup}>
+        <form className={styles.signupForm}>
+        <div className={styles.title}><h1>Sign Up</h1></div>
+            <div className={styles.formControl}>
                 <input id="first-name" type="text" placeholder="First Name" />
                 <small></small>
                 <span></span>
             </div>
-            <div className="form-control">
+            <div className={styles.formControl}>
                 <input id="last-name" type="text" placeholder="Last Name" />
                 <small></small>
                 <span></span>
             </div>
-            <div className="form-control">
+            <div className={styles.formControl}>
                 <input id="email" type="email" placeholder="Email" />
                 <small></small>
                 <span></span>
             </div>
-            <div className="form-control">
-                <input id="password" type="password" placeholder="Password" />
-                <small></small>
-                <span></span>
-            </div>
-            <div className="form-control">
+            <div className={styles.formControl}>
                 <input id="username" type="text" placeholder="Username" />
                 <small></small>
                 <span></span>
             </div>
-            <div className="form-control">
+            <div className={styles.formControl}>
+                <input id="password" type="password" placeholder="Password" />
+                <small></small>
+                <span></span>
+            </div>
+            <div className={styles.formControl}>
                 <input id="address" type="text" placeholder="Address" />
                 <small></small>
                 <span></span>
             </div>
-            <div className="form-control">
+            <div className={styles.formControlGender}>
                 <label>
                 <input type="radio" name="gender" value="male" /> Nam
                 </label>
                 <label>
                 <input type="radio" name="gender" value="female" /> Nữ
                 </label>
-                <label>
-                <input type="radio" name="gender" value="other" /> Khác
-                </label>
                 <small></small>
                 <span></span>
             </div>
-            <div className="form-control">
+            <div className={styles.formControl}>
                 <input id="phone" type="tel" placeholder="Phone number" />
                 <small></small>
                 <span></span>
             </div>
+            <button type="submit" className={styles.submitButton}>Log In</button>
+            <div className={styles.loginNow}><span>Already have an account? </span><Link to="/login" className={styles.loginLink}>Log In now!</Link></div>
         </form>
         </div>
     );
