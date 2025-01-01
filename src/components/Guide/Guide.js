01bacf8ee6d334f7f7197e2f6e82e9e554d065c9
g15 5511 <phuonghaleduy@gmail.com>
2025-01-02 01:25:05 +0700

    Trang hướng dẫn đođo

--- src/components/Guide/Guide.js
+++ src/components/Guide/Guide.js
@@ -1,5 +1,6 @@
 import React from "react";
-import guidePic from '../../guide.png';
+import sensor from '../../AD8232.jpg';
+import equip from '../../howtoequip.png'
 import './Guide.css';
 
 function Guide() {
@@ -12,18 +13,41 @@ function Guide() {
                 </div>
                 <div className="guide-content">
                     <ul className="index-list">
-                        <li>1. Thông tin thiết bị</li>
-                        <li>2. Hướng dẫn lắp đặt</li>
-                        <li>3. Một số lưu ý khi đo</li>
+                        <li><a href="#setup-guide">I.   Thông tin thiết bị</a></li>
+                        <li><a href="#equipment-info">II.  Hướng dẫn lắp đặt</a></li>
+                        <li><a href="#attention">III. Một số lưu ý khi đo</a></li>
                     </ul>
-                    <div className="equipment-info">
-
+                    <div id="equipment-info">
+                        <p className="main-title">I. Thông tin thiết bị</p>
+                        <div className="equipment-info-content">
+                            <div>
+                                <img src={sensor} alt="Cảm biến nhịp tim AD8232" />
+                                <div className="img-content">Cảm biến nhịp tim AD8232</div>
+                                <p className="sub-title1">Thông số kỹ thuật:</p>
+                                <p>Điện áp hoạt động: 2.0V – 3.5V (ổn định nhất ở 3.3V).</p>
+                                <p>Tín hiệu đầu ra: Analog.</p>
+                                <p>Điện năng tiêu thụ thấp: 170 µA (ở điện áp 3V).</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="setup-guide">
-
+                    <div id="setup-guide">
+                        <p className="main-title">II. Hướng dẫn lắp đặt</p>
+                        <div className="setup-guide-content">
+                            <div>
+                                <img src={equip} alt="Hướng dẫn cách lắp đặt thiết bị đo" />
+                                <div className="img-content">Sơ đồ lắp đặt thiết bị đo nhịp tim</div>
+                                <p>Miếng dán 1 (RA – Right Arm): Đặt ở ngực phía trên bên phải, gần xương đòn (clavicle), ngay bên dưới vai phải.</p>
+                                <p>Miếng dán 2 (LA – Left Arm): Đặt ở ngực phía trên bên trái, gần xương đòn (clavicle), ngay bên dưới vai trái.</p>
+                                <p>Miếng dán 3 (RL/LL – Ground/Earth): Đặt ở phía dưới lồng ngực bên trái, gần hông trái (khu vực xương sườn dưới hoặc phần mềm của bụng trái).</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="attention">
-
+                    <div id="attention">
+                        <p className="main-title">III. Một số lưu ý khi đo</p>
+                        <div className="attention-content">
+                            <p>Tín hiệu điện tâm đồ có thể khá nhiễu do hoạt động cơ bắp xung quanh.</p>
+                            <p>Để cải thiện chất lượng tín hiệu cần giữ miếng đệm ở đúng vị trí, không di chuyển quá nhiều trong khi đo, làm sạch khu vực dán cũng như sử dụng miếng đệm mới cho mỗi lần đo.</p>
+                        </div>
                     </div>
                 </div>
             </div>
